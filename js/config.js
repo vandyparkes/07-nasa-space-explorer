bed119582b4a0bbf0097fdf785c3ba0c1509b51a
vandyparkes <vparkes@asu.edu>
2026-04-05 15:25:54 -0500

    add js/config.js .gitignore

---
+++ js/config.js
@@ -0,0 +1,3 @@
+// NASA APOD key. DEMO_KEY works on GitHub Pages with low rate limits (see https://api.nasa.gov/).
+// For higher limits, request a key and replace the value below (do not commit private keys to a public repo).
+const NASA_API_KEY = 'DEMO_KEY';
